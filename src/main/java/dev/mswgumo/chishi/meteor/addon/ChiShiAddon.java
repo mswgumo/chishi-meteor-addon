750fc0201c8920f922a989c886b90e1a6b3ba7da
孤墨 <155886795+mswgumo@users.noreply.github.com>
2026-04-04 18:26:02 +0800

    init: 初始化项目（导入模板并修改包名和元数据）

---
+++ src/main/java/dev/mswgumo/chishi/meteor/addon/ChiShiAddon.java
@@ -0,0 +1,44 @@
+package dev.mswgumo.chishi.meteor.addon;
+
+import com.mojang.logging.LogUtils;
+import meteordevelopment.meteorclient.addons.GithubRepo;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import org.slf4j.Logger;
+
+public class ChiShiAddon extends MeteorAddon {
+    public static final Logger LOG = LogUtils.getLogger();
+    public static final Category CATEGORY = new Category("chishi");
+    public static final HudGroup HUD_GROUP = new HudGroup("chishi");
+
+    @Override
+    public void onInitialize() {
+        LOG.info("Initializing ChiShi Meteor Addon");
+
+        // Modules
+//        Modules.get().add(new ModuleExample());
+
+        // Commands
+//        Commands.add(new CommandExample());
+
+        // HUD
+//        Hud.get().register(HudExample.INFO);
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(CATEGORY);
+    }
+
+    @Override
+    public String getPackage() {
+        return "dev.mswgumo.chishi.meteor.addon";
+    }
+
+    @Override
+    public GithubRepo getRepo() {
+        return new GithubRepo("mswgumo", "chishi-meteor-addon");
+    }
+}
