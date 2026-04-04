26cf1640da2d0385a17a2ec110fef5b5a0c63a41
孤墨 <155886795+mswgumo@users.noreply.github.com>
2026-04-05 03:59:38 +0800

    feat: 添加ShortRangeMixin来实现攻击距离变短

---
+++ src/main/java/dev/mswgumo/chishi/meteor/addon/mixin/ShortRangeMixin.java
@@ -0,0 +1,22 @@
+package dev.mswgumo.chishi.meteor.addon.mixin;
+
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.Entity;
+
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(ClientPlayerInteractionManager.class)
+public class ShortRangeMixin {
+    @Inject(method = "attackEntity", at = @At("HEAD"), cancellable = true)
+    private void onAttackEntity(PlayerEntity player, Entity target, CallbackInfo ci){
+        if (player.distanceTo(target) > 3) {
+            ci.cancel();
+        }
+
+    }
+}
