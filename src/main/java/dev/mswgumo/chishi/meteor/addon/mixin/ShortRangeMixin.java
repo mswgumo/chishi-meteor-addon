12fdf1ba38d77627b95ab4e62a9e05abbe730d15
孤墨 <155886795+mswgumo@users.noreply.github.com>
2026-04-05 04:04:24 +0800

    feat: 彻底实现打开ShortRange后攻击距离变短

--- src/main/java/dev/mswgumo/chishi/meteor/addon/mixin/ShortRangeMixin.java
+++ src/main/java/dev/mswgumo/chishi/meteor/addon/mixin/ShortRangeMixin.java
@@ -1,5 +1,7 @@
 package dev.mswgumo.chishi.meteor.addon.mixin;
 
+import dev.mswgumo.chishi.meteor.addon.modules.ShortRange;
+import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.entity.Entity;
 
@@ -9,11 +11,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Objects;
+
 
 @Mixin(ClientPlayerInteractionManager.class)
 public class ShortRangeMixin {
     @Inject(method = "attackEntity", at = @At("HEAD"), cancellable = true)
     private void onAttackEntity(PlayerEntity player, Entity target, CallbackInfo ci){
+        if (Objects.requireNonNull(Modules.get().get(ShortRange.class)).isActive()) return;
+
+
         if (player.distanceTo(target) > 3) {
             ci.cancel();
         }
