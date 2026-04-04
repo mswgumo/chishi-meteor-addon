a857d47c531a35a763b84939a71c3bb7745055a3
孤墨 <155886795+mswgumo@users.noreply.github.com>
2026-04-04 19:49:02 +0800

    fix: 通过判断玩家是否进入世界，变相修复崩端后，boom功能不会关闭导致进入世界会再次崩溃的问题

--- src/main/java/dev/mswgumo/chishi/meteor/addon/modules/Boom.java
+++ src/main/java/dev/mswgumo/chishi/meteor/addon/modules/Boom.java
@@ -11,6 +11,12 @@ public class Boom extends Module {
     }
     @EventHandler
     public void onTick(TickEvent.Pre event) {
+        // 判断玩家是否进入世界，如果没有就关闭
+        // 变相修复一键崩端后，再从加入世界，如果没有关闭Boom功能会再次崩溃的问题
+        if (mc.player == null && mc.world != null) {
+            this.toggle();
+            return;
+        };
         throw new RuntimeException("一键崩端");
     }
 }
