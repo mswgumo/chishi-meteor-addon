5f47f63f7bde1e95b2aa8b3e6e3a04dd369df8bf
孤墨 <155886795+mswgumo@users.noreply.github.com>
2026-04-06 17:44:00 +0800

    feat: 彻底实现AntiTotem功能

--- src/main/java/dev/mswgumo/chishi/meteor/addon/modules/AntiTotem.java
+++ src/main/java/dev/mswgumo/chishi/meteor/addon/modules/AntiTotem.java
@@ -5,6 +5,9 @@ import dev.mswgumo.chishi.meteor.addon.ChiShiAddon;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.slot.SlotActionType;
 
 public class AntiTotem extends Module {
     public AntiTotem() {
@@ -12,6 +15,17 @@ public class AntiTotem extends Module {
     }
     @EventHandler
     public void onTick(TickEvent.Pre event) {
-        return;
+        // 防止空指针
+        if (mc.player == null) return;
+        // 判断一下副手是否是图腾
+        ItemStack Offhand = mc.player.getOffHandStack();
+        if (Offhand.getItem() == Items.TOTEM_OF_UNDYING) {
+            mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId,
+                45,
+                1,
+                SlotActionType.THROW,
+                mc.player
+                );
+        }
     }
 }
