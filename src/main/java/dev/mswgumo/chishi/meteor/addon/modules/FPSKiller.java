ca49e5c2147b353a977c24158295ce1211e0afc1
mswgumo <mswgumo@git.mswgumo.com>
2026-04-29 21:53:42 +0800

    fix: 修复了FPSKiller功能启动后崩溃的问题

--- src/main/java/dev/mswgumo/chishi/meteor/addon/modules/FPSKiller.java
+++ src/main/java/dev/mswgumo/chishi/meteor/addon/modules/FPSKiller.java
@@ -1,6 +1,7 @@
 package dev.mswgumo.chishi.meteor.addon.modules;
 
 
+import dev.mswgumo.chishi.meteor.addon.ChiShiAddon;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
