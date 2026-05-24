305d4717d2e89cff8dfebe644e6f4edef8a79b26
mswgumo <mswgumo@git.mswgumo.com>
2026-05-24 22:15:44 +0800

    feat: boom功能添加AntiErrorLog参数

---
+++ src/main/java/dev/mswgumo/chishi/meteor/addon/mixin/MinecraftClientMixin.java
@@ -0,0 +1,38 @@
+/*
+ * MIT License
+
+ * Copyright (c) 2026 mswgumo
+
+ * email: mswgumo@dev.mswgumo.com
+ * GitHub: https://github.com/mswgumo
+ */
+
+package dev.mswgumo.chishi.meteor.addon.mixin;
+
+import com.mojang.logging.LogUtils;
+import dev.mswgumo.chishi.meteor.addon.modules.Boom;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.client.MinecraftClient;
+import org.slf4j.Logger;
+import org.slf4j.Marker;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Objects;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @Redirect(method = "run", at = @At(
+        value = "INVOKE",
+        target = "Lorg/slf4j/Logger;error(Lorg/slf4j/Marker;Ljava/lang/String;Ljava/lang/Throwable;)V"
+    ))
+    private void onError(Logger LOGGER, Marker marker, String string, Throwable throwable) {
+        Boom boom = Modules.get().get(Boom.class);
+        if (boom == null) return;
+        if (boom.isActive() && boom.AntiErrorLog.get()) {
+            return;
+        }
+        LOGGER.error(marker, string, throwable);
+    }
+}
--- src/main/java/dev/mswgumo/chishi/meteor/addon/modules/Boom.java
+++ src/main/java/dev/mswgumo/chishi/meteor/addon/modules/Boom.java
@@ -2,10 +2,19 @@ package dev.mswgumo.chishi.meteor.addon.modules;
 
 import dev.mswgumo.chishi.meteor.addon.ChiShiAddon;
 import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 
 public class Boom extends Module {
+    public Setting<Boolean> AntiErrorLog = settings.getDefaultGroup().add(
+        new BoolSetting.Builder()
+            .name("Anti-Error-Log")
+            .defaultValue(true)
+            .build()
+    );
+
     public Boom() {
         super(ChiShiAddon.CATEGORY, "Boom", "Force the client to crash instantly");
     }
